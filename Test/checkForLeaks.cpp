70c925a6729caf437ba2c08f903d72f5eb9f3415
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:33:17 +0530

    Update checkForLeaks.cpp

--- Test/checkForLeaks.cpp
+++ Test/checkForLeaks.cpp
@@ -11,8 +11,8 @@ int main()
     
   cout << "Entered name is: " << 
            name; 
-  string my_key = "AKIAIOSFODNN7EXAMPLE";
-  string my_key_2 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
+  // string my_key = "AKIAIOSFODNN7EXAMPLE";
+  // string my_key_2 = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY";
   
   return 0;
 }
