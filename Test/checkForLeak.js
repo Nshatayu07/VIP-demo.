baa7df09f34397ee604cba23c9b8da8679733ad2
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:34:24 +0530

    Update checkForLeak.js

--- Test/checkForLeak.js
+++ Test/checkForLeak.js
@@ -2,7 +2,7 @@
     func1.displayName="someName"
     console.log(func1.displayName)
 
-// test_var =  "AKIAIOSFODNN7EXAMPLE"
-// target_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+test_var =  "AKIAIOSFODNN7EXAMPLE"
+target_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 console.log(test_var);
