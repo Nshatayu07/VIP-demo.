da17c776047930fe8854bae715f72279436ca6e1
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:34:36 +0530

    Update checkForLeak.java

--- Test/checkForLeak.java
+++ Test/checkForLeak.java
@@ -2,6 +2,6 @@ class Simple{
     public static void main(String args[]){  
      System.out.println("Hello Java");  
     }  
-    test_variable = AKIAIOSFODNN7EXAMPLE
-    aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+    // test_variable = AKIAIOSFODNN7EXAMPLE
+    // aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 } 
