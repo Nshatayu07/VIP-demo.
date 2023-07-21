3b8ebf6a63ba56848c6c2c7b32c5e26e679e586f
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:31:37 +0530

    Create checkForLeak.java

---
+++ Test/checkForLeak.java
@@ -0,0 +1,7 @@
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+    test_variable = AKIAIOSFODNN7EXAMPLE
+    aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
+} 
