a20f604bf4322166377a8257747da2b72d6b5525
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:44:40 +0530

    Delete checkForLeak.js

--- Test/checkForLeak.js
+++
@@ -1,8 +0,0 @@
- function func1(){}
-    func1.displayName="someName"
-    console.log(func1.displayName)
-
-test_var =  "AKIAIOSFODNN7EXAMPLE"
-target_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
-
-console.log(test_var);
