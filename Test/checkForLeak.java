7ec60966e0a3f10be4fb0fa946b8880313057b00
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:44:59 +0530

    Delete Test directory

--- Test/checkForLeak.java
+++
@@ -1,7 +0,0 @@
-class Simple{  
-    public static void main(String args[]){  
-     System.out.println("Hello Java");  
-    }  
-    // test_variable = AKIAIOSFODNN7EXAMPLE
-    // aws_secret_access_key = wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
-} 
