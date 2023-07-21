6569a1e05f1e4e8e17a5314f387420f8bb4f7c09
Shatayu Nagdeve <95160665+Nshatayu07@users.noreply.github.com>
2023-07-21 11:33:31 +0530

    Update checkForLeak.py

--- Test/checkForLeak.py
+++ Test/checkForLeak.py
@@ -2,7 +2,7 @@ def personal_details():
     name, age = "Simon", 19
     address = "Bangalore, Karnataka, India"
     print("Name: {}\nAge: {}\nAddress: {}".format(name, age, address))
-    personal_id = "AKIAIOSFODNN7EXAMPLE",
-    mapping_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    # personal_id = "AKIAIOSFODNN7EXAMPLE",
+    # mapping_id = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
 
 personal_details()
